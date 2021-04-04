1e41862e07bf92b94be56302553f006590562208
Victor Alejandro Ordoñez <vordonez74@gmail.com>
2021-04-04 08:53:44 -0300

    Prueba de conexion agenda2
    host = "wordpress-db-ale:3306";
    $user = "cucai";
    $pass = "voluntad229";
    $db = "agenda2";

--- conexDBwp.php
+++ conexDBwp.php
@@ -1,7 +1,8 @@
 <?php
 include("conexion.php");
-$solicitud = "select user_login from wp_users";
+$solicitud = "select nombre from data";
 $resultado = mysqli_query($conexion,$solicitud);
+echo "<br>";
 var_dump($resultado);
 
 
--- conexion.php
+++ conexion.php
@@ -1,15 +1,15 @@
 <?php
-/*$
-host = "162.243.166.76";
+$
+host = "wordpress-db-ale:3306";
 $user = "cucai";
 $pass = "voluntad229";
 $db = "agenda2";
-*/
+/*
 $host = "wordpress-db-ale:3306";
 $user = "wordpress";
 $pass = "bcViMnngI7wz";
 $db = "wordpress";
-
+*/
 $conexion = mysqli_connect($host,$user,$pass,$db);
 if(!$conexion){
     die("Fallo de coexion". mysqli_connect_error());
